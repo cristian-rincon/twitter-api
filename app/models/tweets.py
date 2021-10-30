f066ad8430722ce5b31e879fa0f8a96669378757
cristian-rincon <cristian.o.rincon.b@gmail.com>
2021-10-30 14:04:53 -0500

    se crea modelo base de tweets

--- app/models/tweets.py
+++ app/models/tweets.py
@@ -1,5 +1,14 @@
-from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+from pydantic import BaseModel, Field
+from uuid import UUID
 
+from app.models.user import User
 
 class Tweet(BaseModel):
-    ...
+    tweet_id: UUID = Field(...)
+    content : str = Field(..., min_length=1, max_length=140)
+    created_at : datetime = Field(default=datetime.now())
+    updated_at : Optional[datetime] = Field(default=None)
+    by: User = Field(...)
