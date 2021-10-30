81c928c26f461925bc6143cea302b584ea2e18e1
cristian-rincon <cristian.o.rincon.b@gmail.com>
2021-10-30 14:19:42 -0500

    se crea la firma para los paths de usuarios

--- app/models/tweets.py
+++ app/models/tweets.py
@@ -6,9 +6,10 @@ from uuid import UUID
 
 from app.models.user import User
 
+
 class Tweet(BaseModel):
     tweet_id: UUID = Field(...)
-    content : str = Field(..., min_length=1, max_length=140)
-    created_at : datetime = Field(default=datetime.now())
-    updated_at : Optional[datetime] = Field(default=None)
+    content: str = Field(..., min_length=1, max_length=140)
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,30 +1,72 @@
+from typing import List
 from fastapi import APIRouter, Body, HTTPException, Path
 from starlette import status
 
-from app.models.user import User, UserBase, UserCreated
+from app.models.users import User, UserBase
 
 router = APIRouter(
     prefix="/users", tags=["users"], responses={404: {"description": "Not found"}}
 )
 
+mock_users = {
+    1: User(
+        user_id="f5f8c9c0-e8e3-4e7b-b8e0-f9b8c8f9f9f9",
+        username="johndoe",
+        email="johndoe@example.com",
+        first_name="John",
+        last_name="Doe",
+        is_active=True,
+        is_superuser=False,
+        password="hunter21",
+    ),
+    2: User(
+        user_id="f5f8c9c0-e8e3-4e7b-b8e0-f9b8c8f9f9f8",
+        username="janesmith",
+        email="janesmith@example.com",
+        first_name="Jane",
+        last_name="Smith",
+        is_active=False,
+        is_superuser=False,
+        password="abcdefga",
+    ),
+}
 
-@router.post("/", response_model=UserCreated, status_code=status.HTTP_201_CREATED)
-def create_user(user: User = Body(...)):
-    """Create a new user.
-
-    Parameters
-    ----------
-    -   user : User
-
-    Returns
-    -------
-    -   UserCreated
-
-    """
-    return user
+# Signup & Login
 
 
-@router.get("/{user_id}", status_code=status.HTTP_200_OK)
+@router.post(
+    "/signup",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary="Sign up",
+)
+def sign_up(user: User = Body(...)):
+    ...
+
+
+@router.post(
+    "/login", response_model=User, status_code=status.HTTP_200_OK, summary="Login"
+)
+def login(user: User = Body(...)):
+    ...
+
+
+@router.get(
+    "/",
+    response_model=List[User],
+    status_code=status.HTTP_200_OK,
+    summary="List of users",
+)
+def get_users():
+    ...
+
+
+@router.get(
+    "/{user_id}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Get user by id",
+)
 def get_user(
     user_id: int = Path(..., gt=0, title="User ID", description="ID of the user")
 ):
@@ -39,37 +81,19 @@ def get_user(
     -   User
 
     """
-    mock_users = {
-        1: User(
-            id=1,
-            username="johndoe",
-            email="johndoe@example.com",
-            first_name="John",
-            last_name="Doe",
-            age=32,
-            hair_color="brown",
-            is_active=True,
-            is_superuser=False,
-            password="hunter21",
-        ),
-        2: User(
-            id=2,
-            username="janesmith",
-            email="janesmith@example.com",
-            first_name="Jane",
-            last_name="Smith",
-            age=30,
-            hair_color="black",
-            is_active=False,
-            is_superuser=False,
-            password="abcdefga",
-        ),
-    }
+
     if user_id not in mock_users:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Userid not found"
         )
     return {"data": mock_users[user_id]}
+
+
+@router.delete(
+    "/{user_id}", status_code=status.HTTP_204_NO_CONTENT, summary="Delete a user"
+)
+def delete_user():
+    ...
 
 
 @router.put("/{user_id}", status_code=status.HTTP_200_OK)
